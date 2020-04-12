86aa30135a230a1805c0dbc2fa076ecd45186e39
James Cole <james@openstack.org>
2020-04-11 19:10:24 -0500

    Add compareGuesses, updateScore, and advanceRound functions

--- script.js
+++ script.js
@@ -6,4 +6,24 @@ let currentRoundNumber = 1;
 
 const generateTarget = () => {
     return Math.floor(Math.random() * 9);
-};
+} 
+
+function compareGuesses(humanGuess, computerGuess, targetNumber) {
+    if (Math.abs(humanGuess - targetNumber) < Math.abs(computerGuess - targetNumber) || humanGuess === computerGuess) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const updateScore = winner => {
+    if (compareGuesses === true) {
+        humanScore += 1;
+    } else if (compareGuesses === false) {
+        computerScore += 1;
+    }
+}
+
+function advanceRound() {
+    currentRoundNumber += 1;
+}
